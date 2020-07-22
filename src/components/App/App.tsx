697c22ab041aeeef3bc5edd58352ef80005e0aba
Dudarev Vyacheslav <slaviknskfordevelop@gmail.com>
2020-07-22 14:09:36 +0700

    commit-1 refactoring

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import types
 // import components
 import MyTable from '../MyTable/MyTable';
 // other imports
 import './App.scss';
+import { ColumnsType } from 'antd/lib/table';
+import { TDataItem } from '../../utils/types';
 
-function App() {
+const columns: ColumnsType<TDataItem> = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+
+const App = () => {
+  const [data, setData] = useState<TDataItem[]>([
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+  ]);
+
+  const onAddHandler = () => {
+    const newData: TDataItem = {
+      key: data.length + 1,
+      name: `Edward King ${data.length + 1}`,
+      age: 32,
+      address: `London, Park Lane no. ${data.length + 1}`,
+    };
+    setData([newData, ...data]);
+  };
+
   return (
     <div className="App">
       <header>
         <h1>Test Ant Design table</h1>
       </header>
 
-      <MyTable />
+      <MyTable data={data} columns={columns} onAdd={onAddHandler} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/MyTable/MyTable.tsx
+++ src/components/MyTable/MyTable.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 // import types
 import { ColumnsType } from 'antd/lib/table';
 import { TDataItem } from './../../utils/types';
@@ -8,60 +8,20 @@ import { Table, Button } from 'antd';
 // other imports
 // import s from './MyTable.module.scss';
 
-export interface IProps {}
+export interface IProps {
+  data: TDataItem[];
+  columns: ColumnsType<TDataItem>;
+  onAdd(): void;
+}
 
 /**
  * Компонент
  */
 const MyTable: FC<IProps> = (props): JSX.Element => {
-  //const { dataSource, columns } = props;
-
-  const columns: ColumnsType<TDataItem> = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-  ];
-  const [data, setData] = useState<TDataItem[]>([
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-    },
-  ]);
+  const { data, columns, onAdd } = props;
 
   const addHandler = () => {
-    const newData: TDataItem = {
-      key: data.length + 1,
-      name: `Edward King ${data.length + 1}`,
-      age: 32,
-      address: `London, Park Lane no. ${data.length + 1}`,
-    };
-    setData([newData, ...data]);
+    onAdd();
   };
 
   return (
